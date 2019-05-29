3d43ec5cc67c0815e3511a2c6708c8add8417be8
Bradley Khan <github@bradleykhan.com>
2019-05-29 17:01:46 +0100

    Set up initial project scaffold

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-    return <div>Page One</div>;
-};
-
-const PageTwo = () => {
-    return <div>Page Two</div>;
-};
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
+                <div>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route
+                        path="/streams/delete"
+                        exact
+                        component={StreamDelete}
+                    />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                </div>
             </BrowserRouter>
         </div>
     );
